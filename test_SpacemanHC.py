a0ec61a14cd81bfea326a59496c87c6d0156b829
KitsuneNoctus <theceintiant@gmail.com>
2019-09-18 21:26:36 -0700

    Made as  way to test further. Not used

---
+++ test_SpacemanHC.py
@@ -0,0 +1,7 @@
+
+def test_is_guess_in_word():
+    test_word = "dodge"
+    test_guess = "d"
+    is_guess_in_word(test_guess, test_word)
+    test_guess = "y"
+    is_guess_in_word(test_guess, test_word)
